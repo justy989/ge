9cddcf1c7ea499b8f915f65294ddb844f4cccc5b
Robert McShane <robert.mcshane@redlinetrading.com>
2017-05-24 11:41:27 -0400

    Benchmark DrawBuffer

--- draw_test.go
+++ draw_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/nsf/termbox-go"
 	"io"
 	"log"
 	"os"
@@ -20,5 +21,46 @@ func BenchmarkNewHighlighter(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		highlighter := NewHighlighter(buffer)
 		highlighter.Highlight(Point{0, 0})
+	}
+}
+
+// benchmark drawing buffer.go
+func BenchmarkDrawBuffer(b *testing.B) {
+	var f io.Reader
+	f, err := os.Open("buffer.go")
+	if err != nil {
+		log.Fatalf("Open() error: %v", err)
+	}
+	defer f.(io.ReadCloser).Close()
+
+	err = termbox.Init()
+	if err != nil {
+		return
+	}
+	defer termbox.Close()
+
+	buffer := &BaseBuffer{}
+	Load(buffer, f)
+
+	terminal_dimensions := Point{}
+	terminal_dimensions.x, terminal_dimensions.y = termbox.Size()
+
+	tabs := TabListLayout{}
+	tabs.tabs = append(tabs.tabs, TabLayout{})
+	current_tab := &tabs.tabs[tabs.selection]
+	root_layout := ViewLayout{}
+	root_layout.view.buffer = buffer
+	current_tab.root = &root_layout
+	current_tab.selection = current_tab.root
+
+	settings := Settings{draw: DrawSettings{4}}
+
+	full_view := Rect{0, 0, terminal_dimensions.x, terminal_dimensions.y}
+	tabs.CalculateRect(full_view)
+
+	for i := 0; i < b.N; i++ {
+		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		tabs.Draw(terminal_dimensions, &settings.draw)
+		termbox.Flush()
 	}
 }
